8755bcfc9233fc329d47ef275ee8829edffa54b4
savannahvaith <sav.vaithilingam@gmail.com>
2021-05-05 18:28:19 +0100

    SV: Cleaned up Service class

--- src/main/java/com/qa/marvel/service/MarvelService.java
+++ src/main/java/com/qa/marvel/service/MarvelService.java
@@ -1,21 +1,46 @@
 package com.qa.marvel.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
 
 import com.qa.marvel.domain.Marvel;
+import com.qa.marvel.repo.MarvelRepo;
 
-public interface MarvelService {
+@Service
+public class MarvelService{
 	
-	// template 
-	// abstract methods - no method body!
+	private MarvelRepo repo;
 	
-	// CRUD OPERATIONS
+	public MarvelService(MarvelRepo repo) {
+		this.repo = repo; 
+	}
 	
-	Marvel create(Marvel m); 
-	List<Marvel> getAll();
-	Marvel getById(Long id); 
-	boolean remove(Long id); 
-	Marvel update(Long id, Marvel newMarvel);
-	Marvel getCharacterByName(String name); 
+	public Marvel create(Marvel m) {
+		return this.repo.save(m);
+	}
+
+	public List<Marvel> getAll() {
+		return this.repo.findAll();
+	}
+
+	public Marvel getById(Long id) {
+		Optional<Marvel> optionalCharacter = this.repo.findById(id);
+		return optionalCharacter.get(); 
+	}
+
+	public boolean remove(Long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
+
+	public Marvel update(Long id, Marvel newMarvel) {
+		return null;
+	}
+
+	public Marvel getCharacterByName(String name) {
+		return null;
+	}
 
 }
