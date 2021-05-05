87d23c55d265b4397b053cbf708cf5bdb0efa309
savannahvaith <sav.vaithilingam@gmail.com>
2021-05-05 11:31:00 +0100

    SV: Showed request mapping example

--- src/main/java/com/qa/marvel/controller/TestController.java
+++ src/main/java/com/qa/marvel/controller/TestController.java
@@ -1,14 +1,23 @@
 package com.qa.marvel.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Allow external access to our app via HTTPRequests
+@RequestMapping("/api")
 public class TestController {
 	
 	@GetMapping("/test") // @TypeOfRequest("location")
 	public String test() {
 		return "Yay, it works";
+	}
+	
+	@GetMapping("/controller")
+	public ResponseEntity<String> getController(){
+		return new ResponseEntity<String>("Hey friends", HttpStatus.OK);
 	}
 
 }
