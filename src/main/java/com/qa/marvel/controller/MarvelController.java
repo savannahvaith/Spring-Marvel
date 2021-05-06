d4c179961572f52210e12708a3cb10433eb9d87c
savannahvaith <sav.vaithilingam@gmail.com>
2021-05-06 11:09:30 +0100

    SV: Added CORS annotation

--- src/main/java/com/qa/marvel/controller/MarvelController.java
+++ src/main/java/com/qa/marvel/controller/MarvelController.java
@@ -6,6 +6,7 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.qa.marvel.domain.Marvel;
 import com.qa.marvel.service.MarvelService;
 
 @RestController
+@CrossOrigin
 public class MarvelController {
 	
 	private MarvelService service; 
